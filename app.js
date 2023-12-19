cb0ab1359b4433019c6109a9f797e46a945a1371
Your Name <you@example.com>
2023-12-19 12:14:47 -0800

    second commit

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const app = express()
-const PORT = process.env.PORT || 300
+const PORT = process.env.PORT || 3000
 
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
+app.get('/students', function (req, res) {
+  var data = [
+    {id:1,name:'Cezar'},
+    {id:2,name:'mhmd'},
+    {id:3,name:'rony'},
+    {id:4,name:'abbess'},
+    {id:5,name:'abu riad'},
+    {id:6,name:'farid'},
+  ]
+  res.send(data)
+})
+
 
 app.listen(PORT)
